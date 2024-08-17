7ae78120ed6f47f554c70f0ec518108d50eaf91e
Kushagra Johari <kushagra_07@Kushagras-MacBook-Air.local>
2024-08-18 01:50:49 +0530

    fix3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 import { FaCheck, FaSkullCrossbones } from "react-icons/fa";
 
-export const successCheckout = {title: 'Great choices!!', subtitle: 'Cheers, checkout Successful!', icon: <div className="sm:mx-0 flex h-12 max-w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 mx-auto sm:h-10 sm:w-10"><FaCheck className='text-xl' /></div>}
-export const failedCheckout = {title: 'Oh snap!!', subtitle: 'Please select a product first', icon: <div className="sm:mx-0 flex h-12 max-w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 mx-auto sm:h-10 sm:w-10"><FaSkullCrossbones className='text-xl' /></div>}
+export const successCheckout = {title: 'Great choices!!', subtitle: 'Cheers, checkout Successful!', icon: <div className="sm:mx-0 flex h-12 max-w-12 flex-shrink-0 items-center justify-center rounded-full sm:h-10 sm:w-10"><FaCheck className='text-xl' /></div>}
+export const failedCheckout = {title: 'Oh snap!!', subtitle: 'Please select a product first', icon: <div className="sm:mx-0 flex h-12 max-w-12 flex-shrink-0 items-center justify-center rounded-full sm:h-10 sm:w-10"><FaSkullCrossbones className='text-xl' /></div>}
